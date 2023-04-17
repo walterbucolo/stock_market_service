a7ed1174cd9e5d623ad248d21787e622bef9600f
Walter Bucolo <bucolowalter@gmail.com>
2023-04-17 14:44:56 -0300

    Improving logging

--- stock_market_app/helpers/logs_settings.py
+++ stock_market_app/helpers/logs_settings.py
@@ -1,13 +1,23 @@
 import logging 
+from datetime import datetime
+import os
+
+
 
 def get_logger():
     # Set up loggings
+    folder_name = 'file_logs'
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    today = str(datetime.now().date())
+    
     logging.basicConfig(
         level=logging.INFO,
         format="[%(asctime)s] [%(levelname)s] %(message)s",
         handlers=[
-            logging.FileHandler("logs/app_three.log"),
+            logging.FileHandler("{}/log-{}.log".format(folder_name, today)),
             logging.StreamHandler()
         ]
     )
+    
     return logging.getLogger(__name__)
